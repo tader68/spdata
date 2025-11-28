314655edf0775345e0eefe559f81f113bf839fa6
tader68 <6at.6868@gmail.com>
2025-11-28 09:58:18 +0700

    set axios baseURL from VITE_API_BASE_URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 import App from './App.jsx'
 import './index.css'
+
+// Cấu hình baseURL cho axios từ biến môi trường Vite
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || ''
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
